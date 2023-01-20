c676bd9451e4da37b33256d49dbdaf954ccf2735
Kandreas9 <andkarido@gmail.com>
2023-01-20 07:49:17 +0100

    installed dev types and cleaned out comments

--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,30 @@
-import * as core from '@actions/core'
-import * as github from '@actions/github'
-import * as fs from 'fs'
+import * as core from "@actions/core";
+import * as github from "@actions/github";
+import * as fs from "fs";
 
-import activityWidget from './widgets/activity'
+import activityWidget from "./widgets/activity";
 
 async function main() {
 	try {
-		const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN')
-		const USERNAME = core.getInput('USERNAME')
-		const TEMPLATE = core.getInput('TEMPLATE')
-		const octokit = github.getOctokit(GITHUB_TOKEN)
+		const GITHUB_TOKEN = core.getInput("GITHUB_TOKEN");
+		const USERNAME = core.getInput("USERNAME");
+		const TEMPLATE = core.getInput("TEMPLATE");
+		const octokit = github.getOctokit(GITHUB_TOKEN);
 
 		const activity = await octokit.rest.activity.listPublicEventsForUser({
 			username: USERNAME,
-		})
+		});
 
-		const source = fs.readFileSync(TEMPLATE, 'utf-8')
-		const reggie = /<!--GITHUB_ACTIVITY-->/g
-		// const matchList = []
+		const source = fs.readFileSync(TEMPLATE, "utf-8");
+		const reggie = /<!--GITHUB_ACTIVITY-->/g;
 
-		// for (let match of source.matchAll(reggie)) {
-		// 	matchList.push(match)
-		// }
+		let activityData = activityWidget(activity.data);
 
-		let activityData = activityWidget(activity.data)
-
-		fs.writeFileSync('README.md', source.replaceAll(reggie, activityData))
+		fs.writeFileSync("README.md", source.replaceAll(reggie, activityData));
 	} catch (err) {
-		core.error(err)
-		process.exit(1)
+		core.error(err);
+		process.exit(1);
 	}
 }
 
-main()
+main();
